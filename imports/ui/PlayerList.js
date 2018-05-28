283b1a585cbd1b7086b3a137dad2d4b102b37401
Amin Zagallai <amza@syntronic.com>
2018-05-28 15:26:52 -0400

    add flip-move 3rd component

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FlipMove from 'react-flip-move';
 
 import Player from './Player';
 
@@ -20,7 +21,9 @@ export default class PlayerList extends React.Component {
     render() {
         return (
             <div>
-                {this.renderPlayers()}
+                <FlipMove maintainContainerHeight={true}>
+                    {this.renderPlayers()}
+                </FlipMove>
             </div>
         );
     }
