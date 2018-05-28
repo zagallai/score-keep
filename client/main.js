220e71b19ddd794e6f288a26afbb216286adbec6
Amin Zagallai <amza@syntronic.com>
2018-05-28 11:02:48 -0400

    separate code into components

--- client/main.js
+++ client/main.js
@@ -6,40 +6,19 @@ import {Tracker} from 'meteor/tracker';
 import {Players} from './../imports/api/players';
 import TitleBar from './../imports/ui/TitleBar';
 import AddPlayer from './../imports/ui/AddPlayer';
-
-const renderPlayers = (playersList) => playersList.map((player) => (
-    <p key={player._id}>
-        {player.name} has {player.score} point(s).
-        <button onClick={()=>handleAdd(event, player._id)}>+1</button>
-        <button onClick={()=>handleSub(event, player._id)}>-1</button>
-        <button onClick={()=>handleRemove(event, player._id)}>X</button>
-    </p>
-));
-
-const handleAdd = (event, playerId) => {
-    Players.update(playerId, {$inc: {score: 1}});
-}
-
-const handleSub = (event, playerId) => {
-    Players.update(playerId, {$inc: {score: -1}});
-}
-
-const handleRemove = (event, playerId) => {
-    Players.remove(playerId);
-}
+import PlayerList from './../imports/ui/PlayerList';
 
 Meteor.startup(() => {
-    Tracker.autorun(() => {
-        let players = Players.find().fetch();
-
-        let title = 'Score Keep App';
-        let jsx = (
-            <div>
-                <TitleBar title={title}/>
-                {renderPlayers(players)}
-                <AddPlayer score={50}/>
-            </div>
-        );
-        ReactDOM.render(jsx, document.getElementById('app'));    
-    });
+  Tracker.autorun(() => {
+    let players = Players.find().fetch();
+    let title = 'Score Keep';
+    let jsx = (
+      <div>
+        <TitleBar title={title} />
+        <PlayerList players={players}/>
+        <AddPlayer/>
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
 });
--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import Player from './Player';
 
 export default class PlayerList extends React.Component {
-    renderPlayers() {
+     renderPlayers() {
         return this.props.players.map((player) => {
-            return <Player key={player._id} player={player} /> 
+            return <Player key={player._id} player={player}/>;
         });
     }
-
     render() {
         return (
             <div>
-                {/* {this.renderPlayers()} */}
-                <h1>Hello</h1>
+                {this.renderPlayers()}
             </div>
-        )
+        );
     }
-}
+};
 
 PlayerList.propTypes = {
-    //players: PropTypes.array.isRequired
+    players: PropTypes.array.isRequired
 }
