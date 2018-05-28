8ccf1d8d306769347fbe4ecf514eb8b4f1820195
Amin Zagallai <amza@syntronic.com>
2018-05-28 11:42:11 -0400

    sort players  based on score

--- client/main.js
+++ client/main.js
@@ -9,7 +9,7 @@ import App from './../imports/ui/App';
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
-    let players = Players.find().fetch();
+    let players = Players.find({}, {sort: {score: -1}}).fetch();
     let title = 'Score Keep';
     ReactDOM.render(<App title={title} players={players} />, document.getElementById('app'));
   });
