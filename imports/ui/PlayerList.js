73ff19f07423da8c478647c6208741fb855158a2
Amin Zagallai <amza@syntronic.com>
2018-05-28 11:09:07 -0400

    add conditional rendering

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -5,9 +5,17 @@ import Player from './Player';
 
 export default class PlayerList extends React.Component {
      renderPlayers() {
-        return this.props.players.map((player) => {
-            return <Player key={player._id} player={player}/>;
-        });
+         if (this.props.players.length) {
+            return this.props.players.map((player) => {
+                return <Player key={player._id} player={player}/>;
+            });    
+         } else {
+             return (
+                <div>
+                    <h4>Please add a player...</h4>
+                </div>
+             );
+         }
     }
     render() {
         return (
