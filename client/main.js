0b63a2d6ce580b9abade81d39e88a97b2e8bb34d
Amin Zagallai <amza@syntronic.com>
2018-05-28 11:29:29 -0400

    add App component

--- client/main.js
+++ client/main.js
@@ -4,21 +4,13 @@ import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
-import PlayerList from './../imports/ui/PlayerList';
+import App from './../imports/ui/App';
+
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find().fetch();
     let title = 'Score Keep';
-    let jsx = (
-      <div>
-        <TitleBar title={title} subtitle='' />
-        <PlayerList players={players}/>
-        <AddPlayer/>
-      </div>
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(<App title={title} players={players} />, document.getElementById('app'));
   });
 });
